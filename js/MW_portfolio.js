91e5a5827a8e65aea8391f885f89708099a4cddc
Mason Wu <masonwu8462@yahoo.com>
2018-06-07 14:23:28 -0400

    Fix: Attempt to correct gif animation on project hovers

--- js/MW_portfolio.js
+++ js/MW_portfolio.js
@@ -1,17 +1,22 @@
 (function hoverProject() {
-  const project1 = document.querySelector('.project-preview1');
-  const project2 = document.querySelector('.project-preview2');
-  const project3 = document.querySelector('.project-preview3');
+  const project1 = document.querySelector('.project1');
+  const projectPreview1 = document.querySelector('.project-preview1');
 
-  project1.addEventListener('animationstart', function() {
-    project1.setAttribute('src', 'images/memory game gif.gif');
+  const project2 = document.querySelector('.project2');
+  const projectPreview2 = document.querySelector('.project-preview2');
+
+  const project3 = document.querySelector('.project3');
+  const projectPreview3 = document.querySelector('.project-preview2');
+
+  project1.addEventListener('mouseover', function() {
+    projectPreview1.setAttribute('src', 'images/memory game gif.gif');
   });
 
-  project2.addEventListener('animationstart', function() {
-    project2.setAttribute('src', 'images/frogger game gif.gif');
+  project2.addEventListener('mouseover', function() {
+    projectPreview2.setAttribute('src', 'images/frogger game gif.gif');
   });
 
-  project3.addEventListener('animationstart', function() {
-    project3.setAttribute('src', 'images/feedreader gif.gif');
+  project3.addEventListener('mouseover', function() {
+    projectPreview3.setAttribute('src', 'images/feedreader gif.gif');
   });
 })();
